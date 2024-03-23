dcc6e0378a3859c11bacea92ceb3bc82d03e518e
mdaminul1024 <mdfahad1024@gmail.com>
2024-03-23 23:45:13 +0600

    insert services data from database

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// car_doctor_server
-// 4J4VqyxGczuLkucz
-
-console.log(process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cm7riuy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -27,8 +23,18 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db("carDoctor").collection("services");
+    
+    app.get('/services', async(req, res)=>{
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
